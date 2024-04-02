140c744032b3c6cb794f060d013c479f8b4155a3
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-04-02 11:46:15 -0500

    declaring variables

---
+++ chapter1/variables.hs
@@ -0,0 +1,12 @@
+import Data.Function (on)
+helloWorld = "Hello, World"
+number5 = 5
+snake_case_variable = True
+number5' = 6
+five = 5
+number = five
+one = 1
+two = one + 1
+two' = two + 1
+two'' = two' + two
+two''' = two'' + two'
